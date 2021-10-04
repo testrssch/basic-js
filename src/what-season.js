fb3efa9e5068824e62fd55e69b23715abf5b1bf3
Andrey Krivosheev <gamedevlawyer@gmail.com>
2021-10-04 22:39:06 +0200

    feat: add what-season.js solution

--- src/what-season.js
+++ src/what-season.js
@@ -2,16 +2,39 @@ import { NotImplementedError } from '../extensions/index.js';
 
 /**
  * Extract season from given date and expose the enemy scout!
- * 
+ *
  * @param {Date | FakeDate} date real or fake date
  * @returns {String} time of the year
- * 
+ *
  * @example
- * 
+ *
  * getSeason(new Date(2020, 02, 31)) => 'spring'
- * 
+ *
  */
-export default function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+ export default function getSeason(date) {
+  if (!date) return 'Unable to determine the time of year!';
+  try {
+    date.getTime();
+  } catch (error) {
+    throw new Error('Invalid date!');
+  }
+    let month = date.getMonth();
+    switch (month) {
+      case 11:
+      case 0:
+      case 1:
+        return "winter";
+      case 2:
+      case 3:
+      case 4:
+        return "spring";
+      case 5:
+      case 6:
+      case 7:
+        return "summer";
+      case 8:
+      case 9:
+      case 10:
+        return "fall";
+    }
+  }
