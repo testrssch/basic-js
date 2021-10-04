a5350e932c2f8c9d391604522835a54c0e6395f4
Andrey Krivosheev <gamedevlawyer@gmail.com>
2021-10-04 22:17:48 +0200

    feat: add dream-team.js solution

--- src/dream-team.js
+++ src/dream-team.js
@@ -2,18 +2,26 @@ import { NotImplementedError } from '../extensions/index.js';
 
 /**
  * Create name of dream team based on the names of its members
- *  
- * @param {Array} members names of the members 
+ *
+ * @param {Array} members names of the members
  * @return {String | Boolean} name of the team or false
  * in case of incorrect members
  *
  * @example
- * 
+ *
  * createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']) => 'ADMM'
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-export default function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ export default function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false;
+  }
+  let name = "";
+  for (let i = 0; i < members.length; i++) {
+    if (typeof (members[i]) == "string") {
+      name += members[i].trim()[0];
+    }
+  }
+  return name.toUpperCase().split("").sort().join("");
 }
